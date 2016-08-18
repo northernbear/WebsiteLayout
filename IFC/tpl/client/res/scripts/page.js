936fe69d550ab5e93040affb8618027d30101614
Bryan Lee <mtcorpvn@gmail.com>
2016-08-18 08:08:01 +0700

    IFC_17082016

--- IFC/tpl/client/res/scripts/page.js
+++ IFC/tpl/client/res/scripts/page.js
@@ -21,7 +21,11 @@
             nav:true
         },
         600:{
-            items:2,
+            items:3,
+            nav:true
+        },
+		768:{
+            items:4,
             nav:true
         },
         960:{
@@ -53,6 +57,10 @@
             items:2,
             nav:true
         },
+		768:{
+            items:3,
+            nav:true
+        },
         960:{
             items:4,
             nav:true
@@ -72,4 +80,45 @@
       }          
      });
     $('.build .build-content').css('min-height',temp);
+	$(".nav .btn-menu").click(function(){
+		$(".nav ul li+li").toggle();
+	});
+	/*$(".nav .btn-menu").click(function(){
+		$(".nav ul li+li").show();
+	});*/
+	// Configure/customize these variables.
+    var showChar = 135;  // How many characters are shown by default
+    var ellipsestext = "...";
+    var moretext = '<img src="tpl/client/res/images/ico/icon_show.png" alt="show"/>';
+    var lesstext = '<img src="tpl/client/res/images/ico/icon_hide.png" alt="show"/>';
+    
+
+    $('.meecontent p').each(function() {
+        var content = $(this).html();
+ 
+        if(content.length > showChar) {
+ 
+            var c = content.substr(0, showChar);
+            var h = content.substr(showChar, content.length - showChar);
+ 
+            var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span><a href="" class="morelink">' + moretext + '</a></span>';
+ 
+            $(this).html(html);
+        }
+ 
+    });
+ 
+    $(".morelink").click(function(){
+        if($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
 });
+
